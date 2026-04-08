c339056448ca0eb9c99157cc0e5a4fd02a89857c
sinan-11 <sinanthaj17@gmail.com>
2026-04-08 10:08:05 +0530

    Time: 0 ms (100%), Space: 54 MB (99.62%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count=0
+    for(let i=0;i<hours.length;i++){
+        if(hours[i]>=target){
+            count++
+        }
+        
+    }
+    return count
+};
+console.log(numberOfEmployeesWhoMetTarget([0,1,2,3,4],2))
