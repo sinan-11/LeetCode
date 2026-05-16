5950acb69f989c111dc4e0cec25cf9fc64a5e7bd
sinan-11 <sinanthaj17@gmail.com>
2026-05-16 10:59:13 +0530

    Time: 0 ms (100%), Space: 61.3 MB (55.68%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -1,12 +1,7 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
 var reverseString = function(s) {
-
-    for(let i = s.length -1; i >=0 ; i--) {
-    s.push(s[i])
-    
-    }
-    for(let j=0;j<s.length;j++){
-        s.shift()
-    }
-    return s
+  s.reverse()
 };
-console.log(reverseString(["h","e","l","l","o"]))
