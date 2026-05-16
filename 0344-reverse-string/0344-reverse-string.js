38872763bd54ec4c5ab937e285981823225024bd
sinan-11 <sinanthaj17@gmail.com>
2026-05-16 10:56:55 +0530

    Time: 600 ms (4.81%), Space: 63.9 MB (5.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -1,7 +1,12 @@
-/**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
- */
 var reverseString = function(s) {
-  s.reverse()
+
+    for(let i = s.length -1; i >=0 ; i--) {
+    s.push(s[i])
+    
+    }
+    for(let j=0;j<s.length;j++){
+        s.shift()
+    }
+    return s
 };
+console.log(reverseString(["h","e","l","l","o"]))
